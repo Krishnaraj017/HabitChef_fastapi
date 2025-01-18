3141664af17e6c19bf219ef640746f4bf254f99e
Krishnaraj Shetty <krishnarajshetty699@gmail.com>
2025-01-18 17:55:43 +0530

    upadte

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import os
 from fastapi import FastAPI
 from app.routes import prediction_router
-import uvicorn
 
 app = FastAPI(title="Food Prediction API")
 
@@ -12,6 +11,3 @@ app.include_router(prediction_router)
 @app.get("/")
 async def root():
     return {"message": "Hello from FastAPI!"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
